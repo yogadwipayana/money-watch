937b4fb7c79ffbf31d0c477855e32ca803c8d065
yogadwipayana <yogadwipayana2006@gmail.com>
2025-09-02 08:12:13 +0800

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  adapter: node({
+    mode: 'standalone' // atau 'server'
+  }),
+  output: 'server', // 🚀 wajib untuk non-prerender
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#111827",
+        darkgreen: "#3ab7bf",
+        bermuda: "#78dcca",
+      },
+    },
+  },
+  plugins: [],
+};
